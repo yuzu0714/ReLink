c4a8da11e8bb2ea70e6f51dbaa0e7c256caa3db4
yuzuki0714 <1237098@st.anan-nct.ac.jp>
2026-09-07 21:11:58 +0900

    追加

--- relink-api/bin/main/Security.kt
+++ relink-api/bin/main/Security.kt
@@ -44,7 +44,7 @@ val aiSimilarityService = AiSimilarityService(
 
 // ★新規追加：Google Geocoding APIを叩くためのサービス
 val geocodingService = GeocodingService(
-    apiKey = securityDotenv["GOOGLE_MAPS_API_KEY"] ?: error("GOOGLE_MAPS_API_KEY が設定されていません")
+    apiKey = securityDotenv.getOrElse("GOOGLE_MAPS_API_KEY") { "" }
 )
 
 
